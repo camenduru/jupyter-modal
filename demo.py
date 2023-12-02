a6c98eed9fa2c3014254f9d121b730235948c68d
camenduru <54370274+camenduru@users.noreply.github.com>
2023-12-02 09:20:15 +0300

    test

--- demo.py
+++ demo.py
@@ -54,5 +54,3 @@ async def run():
 @stub.local_entrypoint()
 def main():
     run.remote()
-    # from notebook.app import main
-    # main(["--port", str(7860), "--ip", "0.0.0.0", "--NotebookApp.token", "", "--no-browser", "--NotebookApp.tornado_settings", "{'headers': {'Content-Security-Policy': 'frame-ancestors *'}}"])
